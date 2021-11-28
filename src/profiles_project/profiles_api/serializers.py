6559c875c7679331f65a8055395824a2efbf2058
mhddanny <mhddanny1@gmail.com>
2021-11-28 10:28:56 +0700

    Added permision and authentication to  our UserProfileViewSet

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -18,7 +18,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Created and return a new user"""
         user = models.UserProfile(
-            email=validated_data['name'],
+            email=validated_data['email'],
             name=validated_data['name']
         )
         user.set_password(validated_data['password'])
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework import response
+from rest_framework import authentication
 from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import  TokenAuthentication
 
 from . import  serializers
 from . import models
+from . import permissions
 
 # Create your views here.
 
@@ -107,5 +110,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles crating and updating profiles."""
     
     serializer_class = serializers.UserProfileSerializer
-    queryset = models.UserProfile.objects.all()    
+    queryset = models.UserProfile.objects.all()
+    authentication_classes  = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+        
     
